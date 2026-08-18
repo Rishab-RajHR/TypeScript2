f174ab70acc798e30f5c1034540cb160f7aad93d
Rishab Raj <rr0948555@gmail.com>
2026-08-18 11:49:12 +0530

    Array

---
+++ dist/array.d.ts
@@ -0,0 +1,1 @@
+declare let data2: (string | number)[];
---
+++ src/array.ts
@@ -0,0 +1,50 @@
+// Array is ordered 
+// Array is used to access the value via index also
+
+// let number = [10,20,30];
+// console.log(number);
+// console.log(number[0]);
+
+
+// Using the TypeScript
+
+// Type[]
+// let marks1 : number[] = [90,80,70];
+// console.log(marks1);
+
+// Array<Type>
+// let user4 : Array<string> = [
+//     "Mohit",
+//      "Rohit"
+// ]
+// console.log(user4);
+
+
+
+// Array with specific data types
+
+// let prices: number[] = [100,200,300]
+// prices.push(111)
+// let cities: string[] = ["Delhi", "Mumbai"]
+// let statusCode: boolean[] = [true,false,true]
+
+
+// Push() / Pop()
+// let fruits: string[] = ["Apple"]
+// fruits.push("Mango")
+// fruits.pop()
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits.length);
+
+
+// MultiType Array
+
+// In this we can use string and number only
+
+let data2: (string | number)[] = [
+       "Mohit",
+       24,
+       "Rohan"
+]
+data2.push(22)
