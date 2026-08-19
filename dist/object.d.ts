f3c68db23486d6fe6f7f576f1328836548a09329
Rishab Raj <rr0948555@gmail.com>
2026-08-19 14:10:05 +0530

    Object

---
+++ dist/object.d.ts
@@ -0,0 +1,8 @@
+declare let products: {
+    productName: string;
+    price: number;
+    address: {
+        city: string;
+        pincode: number;
+    };
+};
---
+++ src/object.ts
@@ -0,0 +1,56 @@
+// let user99: {
+//    userName: string,
+//    age: number
+// }
+
+// user99 = {
+//   userName: "Alex",
+//   age: 28
+// }
+// console.log(user99.userName);
+// console.log(user99.age);
+
+
+
+// Here we have other example
+
+// let products = {
+//     productName: "Laptop",
+//     price: 5000
+// }
+// products.price = 45000;
+// console.log(products.productName);
+// console.log(products.price);
+
+
+// Optional and readonly product
+
+// let products:{
+//     readonly productName: string,
+//     price?: number
+// }
+
+// products = {
+//     productName:"Laptop",
+//     price: 5000
+// }
+
+// products.price = 4500;
+
+// console.log(products.productName);
+// console.log(products.price);
+
+
+
+// Nested Object
+
+let products = {
+   productName: "Laptop",
+   price: 5000,
+   address: {
+       city: "Delhi",
+       pincode: 110056
+   }
+}
+
+console.log(products.address.pincode);
