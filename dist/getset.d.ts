0b4daadf68ac2eafff83ae1d958a25fd83140910
Rishab Raj <rr0948555@gmail.com>
2026-08-23 13:30:48 +0530

    Setter and Getter

---
+++ dist/getset.d.ts
@@ -0,0 +1,6 @@
+declare class User22 {
+    private _name;
+    set name(value: string);
+    get name(): string;
+}
+declare const user6: User22;
---
+++ src/getset.ts
@@ -0,0 +1,38 @@
+// getter and setter is used for fetching private values
+
+// class User22{
+//    public name:string = "";
+// }
+// const user33 = new User22();
+// user33.name = "Alex";
+// console.log(user33.name);
+
+// Getter Method
+// class User22{
+//     private _name = "Alex";
+//     get name(){
+//         return this._name
+//     }
+// }
+// const user6 = new User22();
+// console.log(user6.name);
+
+
+
+// Setter Method
+class User22{
+    private _name = "";
+    set name(value:string){
+      if(value.length < 3){
+          console.log("Name Must be atleast 3 characters");
+          return;
+      };
+       this._name = value;
+    }
+    get name(){
+        return this._name
+    }
+}
+const user6 = new User22();
+user6.name = "Tovino"
+console.log(user6.name);
